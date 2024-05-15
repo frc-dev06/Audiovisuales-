16973eda73b06f9084aa5dfe58d561542a930370
Andres Felipe <felipecortes0604@gmail.com>
2024-05-15 12:55:47 -0500

    avance implementacion de reportes

---
+++ app/src/main/java/com/example/audiovisuales/appInside/reportes/ReportModels.kt
@@ -0,0 +1,4 @@
+package com.example.audiovisuales.appInside.reportes
+
+class ReportModels {
+}
---
+++ app/src/main/java/com/example/audiovisuales/appInside/reportes/ReportesMainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.audiovisuales.appInside.reportes
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.audiovisuales.R
+
+class ReportesMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_reportes_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
